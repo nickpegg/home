d13697d39e7fdbde763b1c086b1a7ae7ea260704
Nick Pegg <nick@nickpegg.com>
2012-11-25 14:26:35 -0500

    Admin inlines are totally awesome

--- brewhouse/admin.py
+++ brewhouse/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from models import *
 
-admin.site.register(Beer)
+class EventInline(admin.TabularInline):
+    model = Event
+
+class BeerAdmin(admin.ModelAdmin):
+    inlines = [EventInline, ]
+    
+admin.site.register(Beer, BeerAdmin)
+
 admin.site.register(Event)
 admin.site.register(Tap)
 admin.site.register(Fermenter)
