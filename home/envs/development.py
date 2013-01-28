f175943c268cc7a0cc5556e4992a56354c3efa71
Nick Pegg <nick@nickpegg.com>
2013-01-27 19:06:00 -0500

    Changed email address in development env

--- home/envs/development.py
+++ home/envs/development.py
@@ -5,7 +5,7 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
-    ('Nick Pegg', 'nick@nickpegg.com'),
+    ('Nick Pegg', 'nope@example.org'),
 )
 
 MANAGERS = ADMINS
