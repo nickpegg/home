86ce8e865f3da8203f1694dce156d5174cabf942
Nick Pegg <nick@nickpegg.com>
2013-02-24 20:24:19 -0500

    Password changing! \o/

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,19 @@ urlpatterns = patterns('',
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/', 'template_name': 'accounts/logout.html'}),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}, name='login'),
     
+    url(r'^password/change/$', 'django.contrib.auth.views.password_change', 
+        {'template_name': 'accounts/pwd_change.html', 'post_change_redirect': '/accounts/profile'}),
+
+    url(r'^password/reset/$', 'django.contrib.auth.views.password_reset', 
+        {'template_name': 'accounts/pwd_reset.html'}),
+    url(r'^password/reset/done/$', 'django.contrib.auth.views.password_reset_done', 
+        {'template_name': 'accounts/pwd_reset_done.html'}),
+    url(r'^password/reset/confirm/$', 'django.contrib.auth.views.password_reset_confirm', 
+        {'template_name': 'accounts/pwd_reset_confirm.html'}),
+    url(r'^password/reset/complete/$', 'django.contrib.auth.views.password_reset_complete',
+        {'template-name': 'accounts/pwd_reset_complete.html'}),
+    
     url(r'^signup/$', 'accounts.views.register'),
     url(r'^profile/$', 'accounts.views.profile'),
+    url(r'^delete/$', 'accounts.views.delete'),
 )
--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib import messages
 
 from accounts.forms import UserForm, NewUserForm
 
 
 def register(request):
+    if request.user.is_authenticated():
+        return redirect('accounts.views.profile')
+        
     form = NewUserForm()
     if request.POST:
         form = NewUserForm(request.POST)
@@ -24,24 +28,37 @@ def register(request):
 @login_required
 def profile(request):
     form = UserForm(instance=request.user)
+    pwd_form = PasswordChangeForm(request.user)
     
     associations = {'twitter': False, 'google_oauth2': False, 'github': False}
     for association in request.user.social_auth.all():
         associations[association.provider.replace('-', '_')] = True
     
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=request.user)
+        if request.POST.get('do_password'):
+            pwd_form = PasswordChangeForm(request.user, request.POST)
+
+            if pwd_form.is_valid():
+                pwd_form.save()
+                messages.success(request, "Password successfully changed.")
+            else:
+                messages.error(request, "Could not update password. See errors below.")
+        elif request.POST.get('do_profile'):
+            form = UserForm(request.POST, instance=request.user)
         
-        if 'delete_user' in request.POST.keys():
-            request.user.delete()
-            return redirect('/')
-        
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'User successfully updated.')
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Profile successfully updated.')
+            else:
+                messages.error(request, 'You have an error in your profile. See below errors.')
         else:
-            messages.error(request, 'You have an error in your profile. See below errors.')
+            messages.error(request, "Er, something weird happened. Contact the site admin.")
         
     
     return render(request, 'accounts/profile.html', locals())
+
+@login_required
+def delete(request):
+    request.user.delete()
+    return redirect('/')
 
