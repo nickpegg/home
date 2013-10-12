e1cef5591a09b845b6f1788cfc2d664437b7325d
Nick Pegg <nick@nickpegg.com>
2013-10-12 19:00:23 -0400

    Minor updates to .gitignore and the fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,13 @@
 from fabric.api import *
+from fabric.contrib.files import *
 
 env.hosts = ['bertha']
+env.base_dir = '/srv/virtual/home.nickpegg.com'
 
 def deploy():
     local('git push deploy')
     
-    with cd('/srv/home/site'):
+    with cd(env.base_dir + '/site'):
         run('git pull')
         
         with prefix('source ../bin/activate'):
