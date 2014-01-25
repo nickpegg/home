4908e62b4d400601172453c6a100839d0671139b
Nick Pegg <nick@nickpegg.com>
2014-01-25 13:54:42 -0500

    Updated fabfile and missing requirements

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,20 @@
 from fabric.api import *
 from fabric.contrib.files import *
 
-env.hosts = ['bertha']
+env.hosts = ['www.home.nickpegg.com']
 env.base_dir = '/srv/virtual/home.nickpegg.com'
 
+
 def deploy():
-    local('git push deploy')
-    
+    local('git push deploy master')
+
     with cd(env.base_dir + '/site'):
         run('git pull')
-        
+
         with prefix('source ../bin/activate'):
+            run('pip install -r requirements.txt')
+            run('./manage.py syncdb')
             run('./manage.py migrate')
-        
+
     sudo('supervisorctl restart home')
+    sudo('supervisorctl restart home_worker')
